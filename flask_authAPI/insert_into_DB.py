021b765fd3fc633f04fe66095e25419637aef9c5
sarimbinwaseem <sarimbinwaseem2000@gmail.com>
2023-01-14 18:07:01 +0500

    Just some work on main UI, not working.

--- flask_authAPI/insert_into_DB.py
+++ flask_authAPI/insert_into_DB.py
@@ -1,7 +1,7 @@
 from run import db, User
 
 username = input("Enter Username: ")
-password = input("Input Password:" )
+password = input("Input Password: " )
 
 db.session.add(User(username = username, password = password))
 db.session.commit()
